f8a41c3968a86b46966eb1921185ee3e240ff1de
lukejans <lukejans5@gmail.com>
2022-12-04 01:22:16 -0500

    Computer and Player Choices Set

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('Hello World');
+function getComputerChoice() {
+  let choices = ['rock', 'paper', 'scissors'];
+
+  let randomNum = Math.floor(Math.random() * choices.length);
+
+  return choices[randomNum];
+}
+console.log(getComputerChoice());
+
+function playerSelection(playerAnswer) {
+  playerAnswer = prompt('pick rock, paper or scissors? ');
+  return playerAnswer.toLowerCase();
+}
+console.log(playerSelection());
