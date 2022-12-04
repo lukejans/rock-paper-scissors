28408b3104191df3c3ac0b79a0b04b8f212fbe16
lukejans <lukejans5@gmail.com>
2022-12-04 01:49:10 -0500

    Player Must Choose

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// STEP 1
 function getComputerChoice() {
   let choices = ['rock', 'paper', 'scissors'];
 
@@ -7,8 +8,18 @@ function getComputerChoice() {
 }
 console.log(getComputerChoice());
 
+// STEP 2
 function playerSelection(playerAnswer) {
   playerAnswer = prompt('pick rock, paper or scissors? ');
-  return playerAnswer.toLowerCase();
+  if (playerAnswer !== null) {
+    return playerAnswer.toLowerCase();
+  } else {
+    playerAnswer = prompt('pick rock, paper or scissors or die! ');
+    if (playerAnswer !== null) {
+      return playerAnswer.toLowerCase();
+    } else {
+      console.log('you life is on the line! you have 6 hours to play or die!');
+    }
+  }
 }
 console.log(playerSelection());
