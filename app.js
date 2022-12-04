e2065e65ed94efd6208d57e5bb502ade05a7011e
lukejans <lukejans5@gmail.com>
2022-12-04 17:55:09 -0500

    Fixed Logic

--- app.js
+++ app.js
@@ -1,25 +1,43 @@
-// STEP 1
+// variables
+
+// game
+function game() {
+  playRound();
+}
+
+// round
+function playRound(playerSelection, computerSelection) {}
+
+const playerSelection = getPlayerChoice();
+const computerSelection = getComputerChoice();
+
+// player choice
+function getPlayerChoice(playerAnswer) {
+  while (playerAnswer == null) {
+    playerAnswer = prompt('pick rock, paper or scissors? ');
+  }
+  return playerAnswer.toLowerCase();
+}
+
+console.log(playerSelection);
+
+// computer choice
 function getComputerChoice() {
-  let choices = ['rock', 'paper', 'scissors'];
-
   let randomNum = Math.floor(Math.random() * choices.length);
-
   return choices[randomNum];
 }
-console.log(getComputerChoice());
 
-// STEP 2
-function playerSelection(playerAnswer) {
-  playerAnswer = prompt('pick rock, paper or scissors? ');
-  if (playerAnswer !== null) {
-    return playerAnswer.toLowerCase();
-  } else {
-    playerAnswer = prompt('pick rock, paper or scissors or die! ');
-    if (playerAnswer !== null) {
-      return playerAnswer.toLowerCase();
-    } else {
-      console.log('you life is on the line! you have 6 hours to play or die!');
-    }
-  }
-}
-console.log(playerSelection());
+const choices = ['rock', 'paper', 'scissors'];
+console.log(computerSelection);
+
+// const playerWins = () => {
+//   player++;
+//   playerScore.innerText = player;
+//   console.log(`Won Round! ${playerSelection} beats ${computerSelection}`);
+// };
+
+// const playerLoses = () => {
+//   computer++;
+//   cpuScore.innerText = computer;
+//   console.log(`Lost Round! ${computerSelection} beats ${playerSelection}`);
+// };
