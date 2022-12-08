ab3b0dede874e0d20536427e669e5b52d1ec730d
lukejans <lukejans5@gmail.com>
2022-12-08 05:03:22 -0500

    Terminal Interactive Display

--- js/terminal.js
+++ js/terminal.js
@@ -1,35 +1,22 @@
-var hackerNameVisible = 0;
-document.onkeypress = function (e) {
-  e = e || window.event;
-  if (e.keyCode == 13) {
-    if (!hackerNameVisible) {
-      hackerNameVisible = 1;
-      var hackerName = getHackerName();
-
-      document.getElementById('hackerName').innerHTML = hackerName;
-      document.getElementById('hackerName').style.opacity = 1;
-      document.getElementById('clear').style.opacity = 1;
-    } else {
-      hackerNameVisible = 0;
-      document.getElementById('hackerName').style.opacity = 0;
-      document.getElementById('clear').style.opacity = 0;
-    }
-  }
-};
-// cursor blink on/off
-document
-  .getElementById('window')
-  .getElementsByClassName('cursor1')[0].innerHTML = '';
-document
-  .getElementById('window')
-  .getElementsByClassName('cursor1')[0].innerHTML = '_';
-
 // start button / btn modals
 const mainContainer = document.querySelector('.main-container');
 const gameContainer = document.querySelector('.game-container');
+const gameDisplay = document.querySelector('.rm-hide');
 const startButton = document.getElementById('start-game-btn');
 
 startButton.addEventListener('click', () => {
   gameContainer.classList.remove('hidden');
   mainContainer.style.display = 'none';
+  gameDisplay.classList.remove('hidden');
+  document
+    .getElementById('window')
+    .getElementsByClassName('cursor1')[0].innerHTML = '';
+  document
+    .getElementById('window')
+    .getElementsByClassName(
+      'inline'
+    )[0].innerHTML = `root@restartGame: Press &lt;RESTART&gt; to reset game session<span
+    class="cursor1"
+    >_</span
+  >`;
 });
