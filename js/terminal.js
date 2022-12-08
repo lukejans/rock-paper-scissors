e6e8ba9bd72df4eb4710b4a84d134c3a84bd9c02
lukejans <lukejans5@gmail.com>
2022-12-08 11:42:27 -0500

    Fixed Favicon

--- js/terminal.js
+++ js/terminal.js
@@ -3,6 +3,14 @@ const mainContainer = document.querySelector('.main-container');
 const gameContainer = document.querySelector('.game-container');
 const gameDisplay = document.querySelector('.rm-hide');
 const startButton = document.getElementById('start-game-btn');
+// statistics / analytics
+const playerDis = document.getElementById('d-pc');
+const compDis = document.getElementById('d-cc');
+const playerDisScore = document.getElementById('d-ps');
+const compDisScore = document.getElementById('d-cs');
+const roundDis = document.getElementById('d-r');
+const winLossDis = document.getElementById('d-w-l');
+const resultDis = document.getElementById('result-txt');
 
 startButton.addEventListener('click', () => {
   gameContainer.classList.remove('hidden');
@@ -14,12 +22,3 @@ startButton.addEventListener('click', () => {
       'inline'
     )[0].innerHTML = `root@restartGame: Press &lt;RESTART&gt; to reset game session`;
 });
-
-// statistics / analytics
-const playerDis = document.getElementById('d-pc');
-const compDis = document.getElementById('d-cc');
-const playerDisScore = document.getElementById('d-ps');
-const compDisScore = document.getElementById('d-cs');
-const roundDis = document.getElementById('d-r');
-const winLossDis = document.getElementById('d-w-l');
-const resultDis = document.getElementById('result-txt');
