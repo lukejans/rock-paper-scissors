e2e72584fd03ba78030e9e7b94ca2d6628f2b14e
lukejans <lukejans5@gmail.com>
2022-12-13 20:01:34 -0500

    Optimized DOM Query

--- js/start.js
+++ js/start.js
@@ -6,28 +6,22 @@ const startButton = document.getElementById('start-game-btn');
 // end button variables
 const restartButton = document.getElementById('restart-btn');
 const restartDisplay = document.querySelector('.play');
+const windowEl = document.getElementById('window');
+const inlineEl = windowEl.getElementsByClassName('inline')[0];
 
 // start button
 startButton.addEventListener('click', () => {
   gameContainer.classList.remove('hidden');
   mainContainer.style.display = 'none';
   gameDisplay.classList.remove('hidden');
-  document
-    .getElementById('window')
-    .getElementsByClassName(
-      'inline'
-    )[0].innerHTML = `root@rockPaperScissors: Game &lt;RUNNING&gt;`;
+  inlineEl.innerHTML = `root@rockPaperScissors: Game &lt;RUNNING&gt;`;
 });
 // end / restart button
 restartButton.addEventListener('click', () => {
   restartDisplay.classList.add('hidden');
   mainContainer.style.display = 'flex';
   gameDisplay.classList.remove('hidden');
-  document
-    .getElementById('window')
-    .getElementsByClassName(
-      'inline'
-    )[0].innerHTML = `root@playGame: Press &lt;PLAY&gt; to start new game<span
+  inlineEl.innerHTML = `root@playGame: Press &lt;PLAY&gt; to start new game<span
     class="cursor1"
     >_</span
   >`;
